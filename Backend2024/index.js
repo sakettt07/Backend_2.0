9395201bb54d8c517298f844d47c042464d5c450
saket <saket@saketgupta.com>
2024-05-14 20:32:05 +0530

    learnt about the middlewares and the parsers.

--- Backend2024/index.js
+++ Backend2024/index.js
@@ -2,14 +2,28 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+// LEarning about Middlewares
+
+app.use(function(req,res,next){
+  console.log("i am middleware");
+  next();
+})
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-app.get('/yt', function (req, res) {
-  res.send('this is my youtube video')
-})
-app.get('/login',function(req,res){
-    res.send('<h1>Please login on the page.</h1>')
+app.get('/profile', function (req, res,next) {
+  return next(new Error("Not implemented"))
 })
 
+
+// Error Handler
+
+app.use((err,req,res,next)=>{
+  console.error(err.stack);
+  res.status(500).send('Something got broke!!')
+})
 app.listen(process.env.PORT)
