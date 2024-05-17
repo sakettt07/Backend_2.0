f47cd87f70810f4adc460ae44b7146b57e228ffa
saket <saket@saketgupta.com>
2024-05-18 01:23:50 +0530

    done with all the basic models and the jwt setup

--- BigBackend/src/models/user.models.js
+++ BigBackend/src/models/user.models.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+// import from "j"
 const userSchema = new mongoose.Schema(
   {
     username: {
@@ -49,4 +52,45 @@ const userSchema = new mongoose.Schema(
     timestamps:true
   }
 );
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password=bcrypt.hash(this.password,10)
+    next();
+    
+})
+// making custom passwchecker method.
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+// making new token generation methods
+userSchema.methods.generateAccess=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.usermame,
+            fullname:this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+    
+}
+userSchema.methods.generateRefresh=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+
+    )
+
+}
+
 export const User = new mongoose.model("User", userSchema);
