8fe18b32225aaadc8c8309ecf485e6cb02b91e20
saket <saket@saketgupta.com>
2024-06-06 13:08:27 +0530

    modifications in usermodels and the register controller

--- BigBackend/src/controllers/user.controller.js
+++ BigBackend/src/controllers/user.controller.js
@@ -5,86 +5,97 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 // method to generate
-const generateAccessAndRefreshToken=async(userid)=>{
+const generateAccessAndRefereshTokens = async(userId) =>{
   try {
-    const user=await User.findById(userid);
-    const accessToken=user.generateAccess();
-    const refreshToken=user.generateRefresh();
-    user.refreshToken=refreshToken
-    await user.save({validateBeforeSave:false})
-    return {accessToken,refreshToken};
+      const user = await User.findById(userId)
+      const accessToken = user.generateAccessToken()
+      const refreshToken = user.generateRefreshToken()
+
+      user.refreshToken = refreshToken
+      await user.save({ validateBeforeSave: false })
+
+      return {accessToken, refreshToken}
+
+
   } catch (error) {
-    throw new ApiError(500,"Something went wrong while generating tokens..")
+      throw new ApiError(500, "Something went wrong while generating referesh and access token")
   }
 }
 
 const registerUser = asyncHandler(async (req, res) => {
-  
-  // get the information from the frontend.
-  // validation kahi koi filed empty toh nhi h
-  //check if user exist toh nhi karta.
-  // check for the images and the avatar
-  // upload them on cloudinary
-  //create user object - In the DB
-  //remove password and refresh token field from the response.
-  //  check for the user creation
-  // return response.
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exists: username, email
+  // check for images, check for avatar
+  // upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return res
 
+  const { fullName, email, username, password } = req.body;
+  //console.log("email: ", email);
 
-//   Validation Begins
-
-  const { fullname, username, email, password } = req.body;
-  if (fullname === "") {
-    throw new ApiError(404, "FullName is required");
-  }
-
-  // the above is a beginners way of writing the validation for all the fields
-  // new approach
   if (
-    [fullname, username, password, email].some((field) => field?.trim() === "")
+    [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All fields are required");
   }
-  // here we have used the $or to find the user on the basis of either email or username
+
   const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
+
   if (existedUser) {
-    throw new ApiError(409, "User already exist");
+    throw new ApiError(409, "User with email or username already exists");
+  }
+  //console.log(req.files);
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
   }
 
-  const avatarLocalPath=req.files?.avatar[0]?.path
-  const coverImageLocalPath=req.files?.avatar[0]?.path
-
-  if(!avatarLocalPath){
-   throw new ApiError(400,"Avatar is required");
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
   }
 
-  const avatar =await uploadOnCloudinary(avatarLocalPath)
-  const coverImage =await uploadOnCloudinary(coverImageLocalPath)
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
-  if(!avatar){
-   throw new ApiError(400,"Avatar is required");
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
   }
- const user = await User.create({
-   fullname,
-   avatar:avatar.url,
-   coverImage:coverImage?.url || "",
-   email,password,username:username.toLowerCase()
-  })
- const createdUser=await User.findById(user._id).select(
-   "-password -refreshToken"
- )
- if(!createdUser){
-   throw new ApiError(500,"Something went wrong while registering the user")
- }
- return res.status(201).json(
-   new ApiResponse(200,createdUser,"User Registered Successfully")
- )
 
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
 
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
-const loginUser=asyncHandler(async (req,res)=>{
+const loginUser = asyncHandler(async (req, res) => {
   // todoss
   // take the data from the req.body (form on the frontend)
   // On what basis you want the user to be logged in username,email.
@@ -92,73 +103,80 @@ const loginUser=asyncHandler(async (req,res)=>{
   // password checker
   // access and refresh token
   // send cookie
-  const {username,email,password}=req.body;
-  if(!username || !email){
-    throw new ApiError(400,"Username and Email is required");
+  const { username, email, password } = req.body;
+  if (!username || !email) {
+    throw new ApiError(400, "Username and Email is required");
   }
-  const user=await User.findOne({
-    $or:[{username},{email}]
-  })
-  if(!user){
-    throw new ApiError(404,"User does not exist");
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
   }
   // pass checker
-  const isPasswordValid=await user.isPasswordCorrect(password);
-  if(!isPasswordValid){
-    throw new ApiError(402,"Invalid User credentials");
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(402, "Invalid User credentials");
   }
 
   // token generation
-  const {accessToken,refreshToken}=await generateAccessAndRefreshToken(user._id);
+  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+    user._id
+  );
 
-  const loggedIn=await User.findById(user._id).select("-password -refreshToken")
+  const loggedIn = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
 
   // setting options for the cookie
-  const options={
-    httpOnly:true,
-    secure:true
-  }
-  return res.status(200).cookie("accessToken",accessToken).cookie("refreshToken",refreshToken).json(
-    new ApiResponse(
-      200,
-      {
-        user:loggedIn,accessToken,refreshToken
-      },
-      "User logged in Successfully"
-    )
-  )
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken)
+    .cookie("refreshToken", refreshToken)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedIn,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in Successfully"
+      )
+    );
+});
 
-} )
-
-const logoutUser=asyncHandler(async(req,res)=>{
+const logoutUser = asyncHandler(async (req, res) => {
   // todos to logout a user
   // find the user by the id bu the prob is from where we get the ID
   // delete its cookies as they are of no use when logged out
   // remove the refresh token from the DB as we will be assingning him the new when he will login again.
   // finally he will be logged out
- const loggedOutUser=await User.findByIdAndUpdate(
+  const loggedOutUser = await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set:{
-        refreshToken:undefined
-      }
-    },{
-      new:true
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
     }
-  )
-  const options={
-    httpOnly:true,
-    secure:true
-  }
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
   return res
-  .status(200)
-  .clearCookie("accessToken",options)
-  .clearCookie("refreshToken",options)
-  .json(new ApiResponse(200,{},"User Logged out Successfully"))
-  
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged out Successfully"));
+});
 
-})
-
-
-// will performing the generate token 
-export { registerUser,loginUser ,logoutUser };
+// will performing the generate token
+export { registerUser, loginUser, logoutUser };
--- BigBackend/src/models/user.models.js
+++ BigBackend/src/models/user.models.js
@@ -1,96 +1,93 @@
-import mongoose, { Schema } from "mongoose";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-// import from "j"
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-      index: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    fullname: {
-      type: String,
-      required: true,
-      lowercase: true,
-      trim: true,
-      index: true,
-    },
-    avatar:{
-        type:String, //cloudinary
-        required:true,
-    },
-    coverImage:{
-        type:String, //cloudinary
-    },
-    watchHistory:[
-        {
-            type:Schema.Types.ObjectId,
-            ref:"Video"
-        }
-    ],
-    password:{
-        type:String,
-        required:[true,'Password is required']
-    },
-    refreshToken:{
-        type:String,
-    }
-  },
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
-  {
-    timestamps:true
-  }
-);
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {
+        timestamps: true
+    }
+)
+
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
-    this.password=await bcrypt.hash(this.password,10)
-    next();
-    
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
 })
-// making custom passwchecker method.
-userSchema.methods.isPasswordCorrect=async function(password){
-    return await bcrypt.compare(password,this.password)
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
 }
-// making new token generation methods
-userSchema.methods.generateAccess=function(){
+
+userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
         {
-            _id:this._id,
-            email:this.email,
-            username:this.usermame,
-            fullname:this.fullname
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
-
     )
-    
 }
-userSchema.methods.generateRefresh=function(){
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
-            _id:this._id,
+            _id: this._id,
+            
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
-
     )
-
 }
 
-export const User = new mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema)
