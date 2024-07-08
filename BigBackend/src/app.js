04ee28ff05a766f1bc5d784b2e30e0259a0f5d59
saket <saket@saketgupta.com>
2024-07-08 21:28:34 +0530

    Started the assignment for all the other routes and the controller just made the empty routes files and linked to the appjs file

--- BigBackend/src/app.js
+++ BigBackend/src/app.js
@@ -3,20 +3,37 @@ const app=express();
 import cors from "cors";
 import cookie_parser from "cookie-parser"
 
-app.use(cors());
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}));
 // in the cors we can soecify the object in which we can write the origin which means the base url.
 
 // setting up different parsers
-app.use(express.json({limit:"18kb"}))
-app.use(urlencoded({extended:true,limit:"18kb"}));
+app.use(express.json({limit:"16kb"}))
+app.use(urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"))
 app.use(cookie_parser());
 
 // Routesss
 
 import userRouter from "./routes/user.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 app.use("/api/v1/users",userRouter);
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 
 
