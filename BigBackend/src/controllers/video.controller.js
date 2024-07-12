aade03c5c6fdd40e2291dc85304e77b6d960297f
saket <saket@saketgupta.com>
2024-07-13 00:31:33 +0530

    video controller and video route completed

--- BigBackend/src/controllers/video.controller.js
+++ BigBackend/src/controllers/video.controller.js
@@ -1,10 +1,11 @@
 import {mongoose ,isValidObjectId} from "mongoose";
-import { Video } from "../models/video.models";
-import {User} from "../models/user.models";
+import { Video } from "../models/video.models.js";
+import {User} from "../models/user.models.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import {v2 as cloudinary} from "cloudinary"
 
 
 const publishAVideo=asyncHandler(async(req,res)=>{
@@ -105,5 +106,101 @@ const getAllVideos=asyncHandler(async(req,res)=>{
 
 
 })
-const getVideoById = asyncHandler(async (req, res) => {})
-export {publishAVideo,getAllVideos,getVideoById};
+const getVideoById = asyncHandler(async (req, res) => {
+    const {videoId}=req.params;
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400,"videoId is not correct");
+    }
+    const video=await Video.findById(videoId);
+    if(!video){
+        throw new ApiError(404,"Video is not found or video id is not correct");
+    }
+    // now we have to increase the views of the video by looking at the watch history.
+    const user=await User.findById(req.user?._id)
+    if(!(user.watchHistory.includes(videoId))){
+        await Video.findByIdAndUpdate(videoId,
+            {
+            $inc:{
+                views:1
+            }
+        },{
+            new:true
+        })
+    }
+    // user ki history me add kardi videoid so it keeps track that the video has been viewed
+    await User.findByIdAndUpdate(req.user?._id,
+        {
+            $addToSet:{
+                watchHistory:videoId
+            }
+        },
+        {new:true}
+    )
+    return res.status(200).json(new ApiResponse(200,video,"Video is fetched by videoId"));
+})
+const updateVideo=asyncHandler(async(req,res)=>{
+    const {videoId}=req.params;
+
+    if(!videoId){
+        throw new ApiError(400,"VideoId is not correct");
+    }
+    const video=await Video.findById(videoId);
+    const publicId=video.publicId;
+    if(!publicId){
+        throw new ApiError(400,"publicId is not present");
+    }
+    if(publicId){
+        try {
+            // cloudinary s delete karni h video or new wali dalni h
+            await cloudinary.uploader.destroy(publicId,{resource_type:"video"})
+        } catch (error) {
+            throw new ApiError(400,"Error while deleting the video file from cloudinary");
+        }
+    }
+    // upload new file pon cloudinary:
+    const videoLocalPath=req.file?.path;
+    if(!videoLocalPath){
+        throw new ApiError(400,"video file is required");
+    }
+    const newVideo=await uploadOnCloudinary(videoLocalPath);
+
+    if(!newVideo.url){
+        throw new ApiError(400,"Error while uploading the video on cloudinary.")
+    }
+    const updateVideo=await Video.findByIdAndUpdate(videoId,
+        {
+            $set:{
+                video:newVideo.url,
+                publicId:newVideo.public_id,
+                duration:newVideo.duration
+            }
+        },
+        {
+            new:true
+        }
+    )
+    return res.status(200).json(new ApiResponse(200,updateVideo,"Video updated successfully"));
+})
+const deleteVideo = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+    //TODO: delete video
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400,"videoid is not correct");
+    }
+
+    const video =await Video.findById(videoId);
+    const publicId=video.publicId;
+    if(publicId){
+        try {
+            await cloudinary.uploader.destroy(publicId,{resource_type:"video"});
+        } catch (error) {
+            throw new ApiError(400,"error while deleting the video from the cloudinary");
+        }
+    }
+    // database m s bhi delete karo.
+    await Video.findByIdAndDelete(videoId);
+    return res.status(200,[],"video is deleted successfully");
+
+})
+
+export {publishAVideo,getAllVideos,getVideoById,updateVideo,deleteVideo};
---
+++ BigBackend/src/routes/video.routes.js
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import {
+    deleteVideo,
+    getAllVideos,
+    getVideoById,
+    publishAVideo,
+    updateVideo,
+} from "../controllers/video.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+import {upload} from "../middlewares/multer.middleware.js"
+
+const router = Router();
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router
+    .route("/")
+    .get(getAllVideos)
+    .post(
+        upload.fields([
+            {
+                name: "videoFile",
+                maxCount: 1,
+            },
+            {
+                name: "thumbnail",
+                maxCount: 1,
+            },
+            
+        ]),
+        publishAVideo
+    );
+
+router
+    .route("/:videoId")
+    .get(getVideoById)
+    .delete(deleteVideo)
+    .patch(upload.single("video"), updateVideo);
+
+
+export default router
